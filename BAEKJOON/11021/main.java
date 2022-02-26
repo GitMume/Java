d120b31d5841ebf25a45f60038ce949538184a7e
Chanyoung Chong <40595857+GitMume@users.noreply.github.com>
2022-02-26 23:47:55 +0900

    2022-02-26

---
+++ BAEKJOON/11021/main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int num = Integer.parseInt(br.readLine());
+        
+        for(int i=1;i<=num;i++){
+            String str = br.readLine();
+            StringTokenizer st = new StringTokenizer(str);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            bw.write("Case #"+i+": "+(a+b)+"\n");
+        }
+        bw.flush();
+        bw.close();
+        
+    }
+}

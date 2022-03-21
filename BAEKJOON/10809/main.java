e097b5ff8ce477c894401050e3d7ac12ae53649d
Chanyoung Chong <40595857+GitMume@users.noreply.github.com>
2022-03-21 23:53:28 +0900

    2022-03-21

---
+++ BAEKJOON/10809/main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[26];
+        
+        for(int i=0; i<arr.length; i++){
+            arr[i]=-1;
+        }
+        
+        String S = sc.nextLine();
+        
+        for(int i=0; i<S.length(); i++){
+            char ch = S.charAt(i);
+            
+            if(arr[ch-'a'] == -1){
+                arr[ch-'a'] = i;
+            }
+            
+        }
+        
+        for(int a:arr){
+            System.out.print(a+" ");
+        }
+    }
+}

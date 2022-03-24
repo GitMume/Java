08b92895a9ced963bcd5b895242af6d047e95478
Chanyoung Chong <40595857+GitMume@users.noreply.github.com>
2022-03-24 21:47:19 +0900

    2022-03-24

---
+++ BAEKJOON/2908/main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        
+        sc.close();
+        
+        A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
+        B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
+        
+        System.out.println(A>B ? A : B);
+
+    }
+}

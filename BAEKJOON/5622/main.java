58f7480c4c0157f29a219f2ed4fa1be1e97b4ff2
Chanyoung Chong <40595857+GitMume@users.noreply.github.com>
2022-03-25 21:17:23 +0900

    2022-03-25

---
+++ BAEKJOON/5622/main.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int cnt=0;
+    
+        for(int i=0;i<s.length();i++){
+            switch(s.charAt(i)){
+                case 'A': case 'B': case 'C':
+                    cnt+=3;
+                    break;
+                case 'D': case 'E': case 'F':
+                    cnt+=4;
+                    break;
+                case 'G': case 'H': case 'I':
+                    cnt+=5;
+                    break;
+                case 'J': case 'K': case 'L':
+                    cnt+=6;
+                    break;
+                case 'M': case 'N': case 'O':
+                    cnt+=7;
+                    break;
+                case 'P': case 'Q': case 'R': case 'S':
+                    cnt+=8;
+                    break;
+                case 'T': case 'U': case 'V':
+                    cnt+=9;
+                    break;
+                case 'W': case 'X': case 'Y': case 'Z':
+                    cnt+=10;
+                    break; 
+            }
+        }
+        System.out.println(cnt);
+    }
+}

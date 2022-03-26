ca5fa1142d6a4bbb9f1461761caf3e6599d51df2
Chanyoung Chong <40595857+GitMume@users.noreply.github.com>
2022-03-26 23:56:50 +0900

    2022-03-26

---
+++ BAEKJOON/2869/main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int V = sc.nextInt();
+        
+        int day = (V-B) / (A-B);
+        
+        if((V-B) % (A-B) != 0){
+            day++;
+        }
+        
+        System.out.println(day);
+    }
+}

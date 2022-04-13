ba2e7615771911157e404c6f8c4315ad4c0fe4c9
Chanyoung Chong <40595857+GitMume@users.noreply.github.com>
2022-04-13 23:56:09 +0900

    2022-04-13

--- BAEKJOON/1065/main.java
+++ BAEKJOON/1065/main.java
@@ -4,14 +4,18 @@
 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력하는 프로그램을 작성하시오.
 -입력: 첫째 줄에 1,000보다 작거나 같은 자연수 N이 주어진다.
 -출력: 첫째 줄에 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력한다.
-
-
 */
 
-import java.io.*;
+import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args) throws IOExceptions{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();        
+        
+    }
+    
+    public static int arithmetic_sequence(int num){
         
     }
 }
